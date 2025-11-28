64e6afac7e58d20511278341b08dbf33bbd549ed
ashutosh24sharma <151420381+ashutosh24sharma@users.noreply.github.com>
2025-11-28 17:28:01 +0530

    page reload problem fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-     <BrowserRouter>
+     <HashRouter>
     <App />
-    </BrowserRouter>
+   </HashRouter>
   </StrictMode>,
 )
